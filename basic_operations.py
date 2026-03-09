8598cd186599e720bdb22852f247be17b1f8e4e3
Michael B Gozo <mbgozobuilds@gmail.com>
2026-03-09 21:51:27 +0000

    Add basic arithmetic operations and print results

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+add = number1 + number2
+minus = number1 - number2
+prod = number1 * number2
+print(f'Addition of {number1} and {number2} is {add}')
+print(f'Subtraction of {number1} and {number2} is {minus}')
+print(f'Multiplication of {number1} and {number2} is {prod}')
